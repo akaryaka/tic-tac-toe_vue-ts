c2f1a4a5fa70a55ef2d2425bf674d4d7d1fe83fd
Andrew <akariaka@yandex.ru>
2026-04-21 14:49:29 +0600

    ref project on tailwind

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -2,16 +2,16 @@
 </script>
 
 <template>
-  <div class="container">
+  <div class="w-full h-screen flex justify-center items-center bg-gradient-to-tr from-white to-[#2e2ed0]">
     <div class="screen-1">
-      <form action="" class="cells-header">
-        <h1 class="cells__title">Кто ходит первым?</h1>
+      <form action="#" class="cells-header border-1 border-solid border-[#9494df] flex flex-col w-[350px] rounded-[10px] bg-[#fff] p-[50px]">
+        <h1 class="cells__title mb-10 align-center text-[25px]">Кто ходит первым?</h1>
         <div class="cells__type-wrapper">
-          <div class="cells__type">
+          <div class="cells__type flex justify-center items-center mb-[10px] w-[40%] uppercase">
             <label for="round">Нолики</label>
             <input name="change" id="round" type="radio" checked />
           </div>
-          <div class="cells__type">
+          <div class="cells__type flex justify-center items-center mb-[10px] w-[40%] uppercase">
             <label for="cross">Крестики</label>
             <input name="change" id="cross" type="radio" />
           </div>
@@ -23,46 +23,4 @@
 </template>
 
 <style lang="scss">
-  .container {
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: linear-gradient(45deg, rgb(255, 255, 255), rgb(46, 46, 208));
-  }
-
-  .cells {
-    width: 100%;
-    margin-bottom: 30px;
-    display: grid;
-    grid-template-columns: repeat(3, 80px);
-    &-header {
-      border: 1px solid #9494df;
-      border-radius: 10px;
-      padding: 50px;
-      display: flex;
-      flex-direction: column;
-      background-color: #fff;
-      width: 350px;
-    }
-    &__type {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      margin-bottom: 10px;
-      width: 40%;
-      text-transform: uppercase;
-    }
-    &__title {
-      text-align: center;
-      margin-bottom: 10px;
-      font-size: 25px;
-    }
-    &-header {
-      &.hide {
-        display: none;
-      }
-    }
-  }
 </style>
