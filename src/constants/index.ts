05eb84a81343f5236c2f7f9df82ada9acf2f2bd1
akariaka@yandex.ru <akariaka@yandex.ru>
2026-06-14 18:21:57 +0600

    add winCheck, refs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,15 +1,26 @@
-  import { ref, reactive } from 'vue';
+import { ref, reactive } from 'vue';
 
-  export const changeValue = ref('round');
-  export const countClick: any = ref(0);
-  export const buttons = reactive([
-    {id: "1", state: false, class: '', classDisabled: ''},
-    {id: "2", state: false, class: '', classDisabled: ''},
-    {id: "3", state: false, class: '', classDisabled: ''},
-    {id: "4", state: false, class: '', classDisabled: ''},
-    {id: "5", state: false, class: '', classDisabled: ''},
-    {id: "6", state: false, class: '', classDisabled: ''},
-    {id: "7", state: false, class: '', classDisabled: ''},
-    {id: "8", state: false, class: '', classDisabled: ''},
-    {id: "9", state: false, class: '', classDisabled: ''},
-  ])
+export const changeValue = ref('round');
+export const countClick: any = ref(0);
+export const buttons = reactive([
+  {id: "1", state: false, class: '', classDisabled: ''},
+  {id: "2", state: false, class: '', classDisabled: ''},
+  {id: "3", state: false, class: '', classDisabled: ''},
+  {id: "4", state: false, class: '', classDisabled: ''},
+  {id: "5", state: false, class: '', classDisabled: ''},
+  {id: "6", state: false, class: '', classDisabled: ''},
+  {id: "7", state: false, class: '', classDisabled: ''},
+  {id: "8", state: false, class: '', classDisabled: ''},
+  {id: "9", state: false, class: '', classDisabled: ''},
+])
+
+export const winCheck = [
+  [1,2,3],
+  [1,4,7],
+  [1,5,9],
+  [2,5,8]
+]
+
+export const displayFirstStep = ref(true);
+export const displaySecondStep = ref(false);
+export const displayThirdStep = ref(false);
