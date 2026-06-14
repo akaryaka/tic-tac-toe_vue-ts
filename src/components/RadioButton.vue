c2fca4f6aa99a97f0f7e8c59c2dba60a54ac266d
akariaka@yandex.ru <akariaka@yandex.ru>
2026-06-14 20:44:26 +0600

    add active class

--- src/components/RadioButton.vue
+++ src/components/RadioButton.vue
@@ -12,13 +12,29 @@
     },
     checked: {
       type: Boolean
-    }
+    },
   })
 </script>
 
 <template>
   <div class="cells__type ">
-    <label :for="value">{{ title }}</label>
-    <input name="change" :value="value" :id="value" type="radio" v-model="changeValue" :checked="checked" />
+    <label :for="value" :class="value === changeValue ? 'label_active' : ''">{{ title }}</label>
+    <input :name="value" :value="value" :id="value" type="radio" v-model="changeValue" :checked="value === changeValue" />
   </div>
 </template>
+
+<style scoped>
+  label {
+    text-transform: uppercase;
+    letter-spacing: 1px;
+  }
+
+  input {
+    opacity: 0;
+  }
+
+  .label_active {
+    border-bottom: 2px solid blue;
+    padding-bottom: 5px;
+  }
+</style>
