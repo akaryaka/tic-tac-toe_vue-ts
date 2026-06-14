f900e8cb7bb013272db1b039d5ff78a1f1657f7a
akariaka@yandex.ru <akariaka@yandex.ru>
2026-06-14 18:20:51 +0600

    add v-show

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -2,14 +2,12 @@
   import FirstStepView from '../views/FirstStepView.vue';
   import SecondStepView from '../views/SecondStepView.vue';
   import ThirdStepView from '../views/ThirdStepView.vue';
+  import { displayFirstStep, displaySecondStep, displayThirdStep } from '../constants';
 </script>
 
 <template>
-  <FirstStepView />
-  <SecondStepView />
-  <ThirdStepView />
+  <FirstStepView v-show="displayFirstStep"/>
+  <SecondStepView v-show="displaySecondStep"/>
+  <ThirdStepView v-show="displayThirdStep"/>
 </template>
 
-<style scoped>
-
-</style>
