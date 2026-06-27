984bbfd1b30c36bee26dc35b0744e99f53f1885c
akariaka@yandex.ru <akariaka@yandex.ru>
2026-06-27 21:19:53 +0600

    add styles

--- src/views/ThirdStepView.vue
+++ src/views/ThirdStepView.vue
@@ -1,14 +1,45 @@
 <script lang="ts" setup>
-
+  import { winner } from '../constants';
 </script>
 
 <template>
-  <div class="container">
+  <Transition>
     <div class="screen-3">
-      <p class="screen-3__output">Победа!</p>
+      <form action="#" class="cells-header">
+        <h2 class="cells__title">Победа!</h2>
+        <p>{{ winner }} победили</p>
+      </form>
     </div>
-  </div>
+  </Transition>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+  .cells {
+    &-header {
+      border: 1px solid #9494df;
+      border-radius: 10px;
+      padding: 50px;
+      display: flex;
+      flex-direction: column;
+      background-color: #fff;
+      width: 450px;
+    }
+    &__title {
+      text-align: center;
+      margin-bottom: 30px;
+    }}
+
+  .v-enter-active {
+    transition: opacity 0.3s ease;
+
+  }
+
+  .v-leave-active {
+    transition: opacity 0.3s ease;
+  }
+
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
 </style>
